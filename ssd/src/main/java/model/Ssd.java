c784053933544caacf96654ba2d6c3538076cec7
Haneol <haneolj@gmail.com>
2024-09-30 12:37:09 +0900

    [feat] add Ssd Entity class

--- ssd/src/main/java/model/Ssd.java
+++ ssd/src/main/java/model/Ssd.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Ssd {
+    private int[] data;
+
+    public int getData(int index) {
+        return data[index];
+    }
+
+    public void setData(int index, int value) {
+        this.data[index] = value;
+    }
 }
